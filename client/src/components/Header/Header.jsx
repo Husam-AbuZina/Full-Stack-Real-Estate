5664940058577a62f01f9bd2e0853b776171ee6d
Husam-AbuZina <husamzinap@gmail.com>
2023-11-18 07:33:51 +0200

    authentication - part 1

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -5,17 +5,21 @@ import { getMenuStyles } from "../../utils/common";
 import useHeaderColor from "../../hooks/useHeaderColor";
 import OutsideClickHandler from "react-outside-click-handler";
 import { Link, NavLink } from 'react-router-dom'
+import { useAuth0 } from '@auth0/auth0-react'
+import ProfileMenu from "../ProfileMenu/ProfileMenu";
+
 
 const Header = () => {
   const [menuOpened, setMenuOpened] = useState(false);
   const headerColor = useHeaderColor();
+  const { loginWithRedirect, isAuthenticated, user, logout } = useAuth0();
 
   return (
     <section className="h-wrapper" style={{ background: headerColor }}>
       <div className="flexCenter innerWidth paddings h-container">
         {/* logo */}
         <Link to="/">
-        <img src="./logo.png" alt="logo" width={100} />
+          <img src="./logo.png" alt="logo" width={100} />
         </Link>
 
         {/* menu */}
@@ -31,10 +35,16 @@ const Header = () => {
           >
             <NavLink to="/properties">Properties</NavLink>
 
-              <a href="mailto:zainkeepscode@gmail.com">Contact</a>
-              <button className="button">
+            <a href="mailto:husamzinap@gmail.com">Contact</a>
+            {/* login button */}
+            {!isAuthenticated ?
+              <button className="button" onClick={loginWithRedirect}>
                 Login
-              </button>
+              </button> :
+              <ProfileMenu user={user} logout={logout} 
+              onClick={() => console.log('Profile Clicked!------------------------------------------------------------------------')}
+              />
+            }
           </div>
         </OutsideClickHandler>
 
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,9 +2,24 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import { Auth0Provider } from '@auth0/auth0-react'
+import { MantineProvider } from '@mantine/core';
+
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <Auth0Provider 
+    domain="dev-gz54j2my6q3a12zm.us.auth0.com"
+    clientId="mXMRS4EsvqhQzMfdZqPjPJ9wmMkkLu2o"
+    authorizationParams={{
+      redirect_uri: "http://localhost:5173"
+    }}
+    audience="http://localhost:8000"
+    scope="openid profile email"
+    >
+          <MantineProvider withGlobalStyles withNormalizeCSS>
+        <App />
+      </MantineProvider>
+    </Auth0Provider>
   </React.StrictMode>
 );
