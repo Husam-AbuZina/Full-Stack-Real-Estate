9d51e6af80c66c278125e46d427f89cae1678052
Husam-AbuZina <husamzinap@gmail.com>
2023-11-22 17:55:37 +0200

    fix bugs

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,34 +1,34 @@
-import React, {Suspense} from 'react'
+import React, { Suspense } from "react";
 import "./App.css";
 import Website from "./pages/Website";
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
-import Layout from './components/Layout/Layout';
-import Properties from './pages/Properties/Properties';
-import Property from './pages/Property/Property';
-import { QueryClient, QueryClientProvider } from 'react-query'
-import { ReactQueryDevtools } from 'react-query/devtools'
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout/Layout";
+import Properties from "./pages/Properties/Properties";
+import Property from "./pages/Property/Property";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
-  const queryClient = new QueryClient()
+  const queryClient = new QueryClient();
   return (
-    <QueryClientProvider client={queryClient} >
-    <BrowserRouter>
-    <Suspense fallback={<div>Loading...</div>}>  
-    <Routes>
-      <Route element={<Layout />}>
-      <Route path="/" element={<Website />} />
-      <Route path="/properties">
-      <Route index element={<Properties />} />
-      <Route path=":propertyId" element={<Property />} />
-      </Route>
-      </Route>
-    </Routes>
-    </Suspense>
-    </BrowserRouter>
-    <ToastContainer />
-    <ReactQueryDevtools initialIsOpen={false} />
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Routes>
+            <Route element={<Layout />}>
+              <Route path="/" element={<Website />} />
+              <Route path="/properties">
+                <Route index element={<Properties />} />
+                <Route path=":propertyId" element={<Property />} />
+              </Route>
+            </Route>
+          </Routes>
+        </Suspense>
+      </BrowserRouter>
+      <ToastContainer />
+      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
 }
--- client/src/components/ProfileMenu/ProfileMenu.jsx
+++ client/src/components/ProfileMenu/ProfileMenu.jsx
@@ -1,19 +1,22 @@
-import React from 'react'
-import {Avatar, Menu} from '@mantine/core'
+import React from "react";
+import { Avatar, Menu } from "@mantine/core";
 
-const ProfileMenu = ({user, logout}) => {
+const ProfileMenu = ({ user, logout }) => {
   return (
     <Menu>
-        <Menu.Target>
-            <Avatar src={user?.picture} alt="user image" radius={"xl"} />
-        </Menu.Target>
-        <Menu.Dropdown>
-            <Menu.Item>
-                Favourites
-            </Menu.Item>
-        </Menu.Dropdown>
+      <Menu.Target>
+        <Avatar src={user?.picture} alt="user image" radius={"xl"} />
+      </Menu.Target>
+      <Menu.Dropdown>
+        <Menu.Item onClick={console.log("Favorite")}>Favourites</Menu.Item>
+        <Menu.Item>test</Menu.Item>
+        <Menu.Item>test</Menu.Item>
+        <Menu.Item>test</Menu.Item>
+        <Menu.Item>test</Menu.Item>
+        <Menu.Item>test</Menu.Item>
+      </Menu.Dropdown>
     </Menu>
-  )
-}
+  );
+};
 
-export default ProfileMenu
+export default ProfileMenu;
